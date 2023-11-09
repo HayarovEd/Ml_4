96bb62240454878054fbd6b5be66c4533332b26a
hayarov <edurda77@gmail.com>
2023-11-09 16:06:18 +0500

    3

---
+++ app/src/main/java/org/zaim/na/kartu/polus/AppPolus.kt
@@ -0,0 +1,23 @@
+package org.zaim.na.kartu.polus
+
+import android.app.Application
+import com.my.tracker.MyTracker
+import com.yandex.metrica.YandexMetrica
+import com.yandex.metrica.YandexMetricaConfig
+import dagger.hilt.android.HiltAndroidApp
+import org.zaim.na.kartu.polus.data.APP_METRICA
+import org.zaim.na.kartu.polus.data.MY_TRACKER
+
+@HiltAndroidApp
+class AppPolus : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        val config = YandexMetricaConfig.newConfigBuilder(APP_METRICA).build()
+
+
+        MyTracker.initTracker(MY_TRACKER, this)
+        YandexMetrica.activate(applicationContext, config)
+        YandexMetrica.enableActivityAutoTracking(this)
+    }
+}
--- app/src/main/java/org/zaim/na/kartu/polus/ui/theme/Color.kt
+++ app/src/main/java/org/zaim/na/kartu/polus/ui/theme/Color.kt
@@ -11,6 +11,8 @@ val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
 val baseBackground = Color(0xFF333652)
+val darkText = Color(0xFF25282B)
 val yellow = Color(0xFFFAD02C)
 val white = Color(0xFFFFFFFF)
+val semiWhite = Color (0xCCFFFFFF)
 val grey = Color(0xFFACAEB6)
